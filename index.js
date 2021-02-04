b8d2cda8373bdda20ddbb7c4489477106641902c
Diogo Malfatti <diimalfatti@gmail.com>
2021-02-04 16:56:28 -0300

    filter files by extension

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const fs = require('./funcoes')
 const caminho = path.join(__dirname, 'legendas')
 
 fs.lerDiretorio(caminho)
+    .then(arquivos => fs.elementosTerminadosCom(arquivos, '.srt'))
     .then(console.log)
