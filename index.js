d0ad463575b20140142c2c6439b1ab5d3ccbeabb
Diogo Malfatti <diimalfatti@gmail.com>
2021-02-04 18:57:38 -0300

    read files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const path = require('path')
-const fs = require('./funcoes')
+const fn = require('./funcoes')
 
 const caminho = path.join(__dirname, 'legendas')
 
-fs.lerDiretorio(caminho)
-    .then(arquivos => fs.elementosTerminadosCom(arquivos, '.srt'))
+fn.lerDiretorio(caminho)
+    .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+    .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
     .then(console.log)
